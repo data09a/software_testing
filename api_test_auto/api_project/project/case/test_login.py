fb64b75ff912f053d110776c9f259f35eecd0467
Annie <tingwu@TINGs-MacBook-Pro.local>
2019-11-29 22:24:53 +0800

    git

---
+++ api_test_auto/api_project/project/case/test_login.py
@@ -0,0 +1,34 @@
+import unittest
+from api.api_login import ApiLogin
+from parameterized import parameterized
+from tools.read_json import ReadJson
+
+
+
+def get_data():
+    data = ReadJson("login.json").read_json()
+    arrs = []
+    arrs.append((data.get("url"),
+                 data.get("mobile"),
+                 data.get("code"),
+                 data.get("expect_result"),
+                 data.get("status_code")))
+    return arrs
+
+
+class TestLogin(unittest.TestCase):
+
+    @parameterized.expand(get_data())
+    def test_login(self, url,mobile, code, expect_result, status_code):
+
+        s = ApiLogin().api_post_login(url, mobile, code)
+
+        print("查看响应结果：", s.json())
+
+        self.assertEquals(expect_result, s.json()['message'])
+
+        self.assertEquals(status_code, s.status_code)
+
+
+if __name__ == '__main__':
+    unittest.main()
